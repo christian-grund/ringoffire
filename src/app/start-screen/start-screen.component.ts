4b686512b0caa923301cd76a8f318633cd544873
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-09 11:06:05 +0200

    05 - Karte animieren

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
   imports: [],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  newGame() {
+    // Start game
+    this.router.navigateByUrl('/game');
+  }
 }

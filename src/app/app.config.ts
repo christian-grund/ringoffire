d73d93743e34d052751a8418ce86ef89d6f8cf65
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-15 10:53:36 +0200

    Init firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'ring-of-fire-ce158',
+          appId: '1:241440440422:web:552020f8df317f4b8a2da7',
+          storageBucket: 'ring-of-fire-ce158.appspot.com',
+          apiKey: 'AIzaSyAMSgbyfYejAlSjr9J_HHqxiqIpP9_jB5g',
+          authDomain: 'ring-of-fire-ce158.firebaseapp.com',
+          messagingSenderId: '241440440422',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
